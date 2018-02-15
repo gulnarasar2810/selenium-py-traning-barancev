8d18dfbf52ed2c65ccf6517314711babb2eba56e
Гульнара Сарварова <sgm@htc-cs.ru>
2018-02-15 11:15:05 +0400

    add tests

---
+++ php4dvd/test_delete_movie.py
@@ -0,0 +1,13 @@
+from model.user import User
+from model.movie import Movie
+#from selenium_fixture import app
+
+def test_delete_movie(app):
+    new_movie = Movie.random()
+    app.ensure_login_as(User.Admin())
+    app.add_movie(new_movie)
+    assert app.is_added_movie(new_movie)
+    app.delete_movie(new_movie)
+    #app.search_exist_movie(new_movie)
+    #assert app.is_deleted_movie(new_movie)
+
